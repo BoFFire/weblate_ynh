efbc55721f45b880083308d7789ba081ccbe3ac6
ButterflyOfFire <42316180+BoFFire@users.noreply.github.com>
2023-04-26 10:09:08 +0100

    Removing forge Github

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -31,19 +31,19 @@ fi
 # PERSONAL HELPERS
 #=================================================
 
-set_forge_variables() {
-	if [ $used_forge = "GitHub" ] ; then
-		github_username="$forge_username"
-		github_token="$forge_token"
-		gitlab_username="None"
-		gitlab_token="None"
-	else
-		github_username="None"
-		github_token="None"
-		gitlab_username="$forge_username"
-		gitlab_token="$forge_token"
-	fi
-}
+#set_forge_variables() {
+#	if [ $used_forge = "GitHub" ] ; then
+#		github_username="$forge_username"
+#		github_token="$forge_token"
+#		gitlab_username="None"
+#		gitlab_token="None"
+#	else
+#		github_username="None"
+#		github_token="None"
+#		gitlab_username="$forge_username"
+#		gitlab_token="$forge_token"
+#	fi
+#}
 
 #=================================================
 # EXPERIMENTAL HELPERS
